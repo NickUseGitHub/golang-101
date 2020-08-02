6078293a293876fbe944e60350a6736837649dd3
Chaiwtun <chaiwtun@scg.com>
2020-08-03 00:16:57 +0700

    [master] init simple app/api

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello world eiei")
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(map[string]string{"Hello": "World"})
+	})
+	http.ListenAndServe(":8080", r)
 }
