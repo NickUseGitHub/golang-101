dcd9176b22f43c4bd62ea91cea863a46ee79872c
Chaiwtun <chaiwtun@scg.com>
2020-08-10 23:34:48 +0700

    [master] assign port as variable

--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	port := 8080
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(map[string]string{"Hello": "World"})
 	})
-	http.ListenAndServe(":8080", r)
+
+	fmt.Printf("App listen on port %d", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), r)
 }
