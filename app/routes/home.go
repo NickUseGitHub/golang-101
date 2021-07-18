2f2cdde2bd825701146ddea544a7b9cdb2dcd75f
Chaiwtun <chaiwtun@scg.com>
2021-07-18 14:09:13 +0700

    [master] refactor code

--- app/routes/home.go
+++ app/routes/home.go
@@ -1,7 +1,6 @@
 package routes
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/jinzhu/gorm"
@@ -9,5 +8,5 @@ import (
 
 // HomeIndex of app
 func HomeIndex(_ *gorm.DB, w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(map[string]string{"Hello": "Wordddld"})
+	w.Write([]byte("Ok"))
 }
